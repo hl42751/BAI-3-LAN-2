2cad1eaf22638c3d9d1a2c6968e3c6f0f7223b4f
hl42751 <104617618+hl42751@users.noreply.github.com>
2022-05-25 15:28:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- <?php 
+      $a = 5;
+      $A = 10;
+      echo $a;
+      echo '<br>';
+      echo $A;
+?> -->
+ 
+<?php 
+    $firstName = 'LE VIET';
+    $lastName = ' HAI HOANG';
+    $fullName = $firstName . ''. $lastName;
+    echo $fullName;
+?>
